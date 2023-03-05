9c7cc62a42ba82161a5fd1677a40efa0d9aa1bf0
tushan-shahid <shahidshabraj@gmail.com>
2023-03-05 12:35:44 -0800

    user signin connected to db

--- signin.php
+++ signin.php
@@ -1,5 +1,32 @@
 <?php
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $mysqli = require __DIR__ . '/database.php';
+
+    $sql = sprintf("SELECT * FROM user WHERE username = '%s'", $mysqli->real_escape_string($_POST['username']));
+
+    $result = $mysqli->query($sql);
+
+    $user = $result->fetch_assoc();
+
+    if ($user) {
+        if (password_verify($_POST['password'], $user['password_hashed'])) {
+            session_start();
+            $_SESSION['user'] = $user;
+            header("Location: postsUser.php");
+            exit;
+        } else {
+            die("Incorrect password!");
+        }
+    } else {
+        die("User does not exist!");
+    }
+
+    // var_dump($user);
+    // exit;
+}
+
 ?>
 
 
