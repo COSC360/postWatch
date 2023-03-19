dd658721c09a7fe2cc332b5e1041c12dcb51f742
tushan-shahid <shahidshabraj@gmail.com>
2023-03-18 17:40:19 -0700

    linked posts to fullPost

--- fullPost.php
+++ fullPost.php
@@ -44,19 +44,19 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <style>
-    .navbar-nav .nav-link:hover {
-        background-color: #3d464e;
-        border-radius: 10%;
+        .navbar-nav .nav-link:hover {
+            background-color: #3d464e;
+            border-radius: 10%;
 
-    }
+        }
 
-    .btn:hover {
-        background-color: #f1f1f1;
-    }
+        .btn:hover {
+            background-color: #f1f1f1;
+        }
 
-    .btn i {
-        margin-right: 5px;
-    }
+        .btn i {
+            margin-right: 5px;
+        }
     </style>
 
     <title>Full-Post</title>
@@ -75,9 +75,7 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
             <a class="navbar-brand" href="#">
                 <img src="./img/logo.png" alt="..." height="80" />
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -104,9 +102,9 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
             <div class="col-md-6 mx-auto text-center">
                 <h1 class="display-4 fst-italic">Full Post</h1>
                 <p class="lead my-3">Read entire content here, like and comment</p>
-                <button class="btn btn-primary" onclick="location.href='userHomepage.php'"><i
-                        class="bi bi-arrow-left"></i> Go
-                    Back Home</button>
+                <button class="btn btn-primary" onclick="history.go(-1)"><i class="bi bi-arrow-left"></i> Go Back
+                </button>
+
             </div>
         </div>
 
@@ -150,7 +148,7 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     </main>
 
     <script>
-    // like post
+        // like post
     </script>
 
 </body>
--- postsFeed.php
+++ postsFeed.php
@@ -41,9 +41,7 @@ if (!isset($_SESSION['user_id'])) {
             <a class="navbar-brand" href="#">
                 <img src="./img/logo.png" alt="..." height="80" />
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -64,37 +62,36 @@ if (!isset($_SESSION['user_id'])) {
             <div class="col-md-6 mx-auto text-center">
                 <h1 class="display-4 fst-italic">Posts Feed</h1>
                 <p class="lead my-3">Discover and read everyone's latest posts here</p>
-                <button class="btn btn-primary" onclick="location.href='userHomepage.php'"><i
-                        class="bi bi-arrow-left"></i> Go
+                <button class="btn btn-primary" onclick="location.href='userHomepage.php'"><i class="bi bi-arrow-left"></i> Go
                     Back Home</button>
 
             </div>
         </div>
         <div class="row mb-2 align-items-stretch">
             <?php foreach ($posts as $post) : ?>
-            <div class="col-md-6">
-                <div
-                    class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                    <div class="col p-4 d-flex flex-column position-static postUser">
-                        <strong class="d-inline-block mb-2 text-primary">
-                            <?php echo $post["username"];
+                <div class="col-md-6">
+                    <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div class="col p-4 d-flex flex-column position-static postUser">
+                            <strong class="d-inline-block mb-2 text-primary">
+                                <?php echo $post["username"];
                                 ?>
-                        </strong>
-                        <h3 class="mb-0"><?php echo $post["title"]; ?></h3>
-                        <div class="mb-1 text-muted"><?php echo date("F j, Y", strtotime($post["date"])); ?></div>
-                        <?php echo substr($post['content'], 0, 100) . '...'; ?>
-                        <a href="#" class="stretched-link">Continue reading</a>
-                        <div class="mt-3 d-flex align-items-center">
-                            <span class="me-4"><i class="bi bi-heart text-danger hover-text-danger"></i></span>
-                            <span class="ms-4"><i class="bi bi-chat-dots text-primary hover-text-primary"></i></span>
+                            </strong>
+                            <h3 class="mb-0"><?php echo $post["title"]; ?></h3>
+                            <div class="mb-1 text-muted"><?php echo date("F j, Y", strtotime($post["date"])); ?></div>
+                            <?php echo substr($post['content'], 0, 100) . '...'; ?>
+                            <a href="fullPost.php?id=<?php echo $post['id']; ?>" class="stretched-link">Continue
+                                reading</a>
+                            <div class="mt-3 d-flex align-items-center">
+                                <span class="me-4"><i class="bi bi-heart text-danger hover-text-danger"></i></span>
+                                <span class="ms-4"><i class="bi bi-chat-dots text-primary hover-text-primary"></i></span>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-6 col-lg-4 d-flex align-items-center p-2">
-                        <img src="<?php echo $post["image"]; ?>" alt="User Image" class="img-fluid" />
-                    </div>
+                        <div class="col-md-6 col-lg-4 d-flex align-items-center p-2">
+                            <img src="<?php echo $post["image"]; ?>" alt="User Image" class="img-fluid" />
+                        </div>
 
+                    </div>
                 </div>
-            </div>
             <?php endforeach; ?>
         </div>
     </main>
