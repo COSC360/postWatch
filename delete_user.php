9d9ab8ecb8f9966d10b49b31b36c418b390c759c
abhimanyukinra <73505956+abhimanyukinra@users.noreply.github.com>
2023-03-18 20:47:03 -0700

    Update delete_user.php

--- delete_user.php
+++ delete_user.php
@@ -1,17 +1,17 @@
 <?php
 session_start();
 if (!isset($_SESSION['admin_id'])) {
-  header("Location: index.php");
-  exit;
+    header("Location: index.php");
+    exit;
 } elseif (isset($_POST['user_id'])) {
-  $mysqli = require __DIR__ . '/database.php';
-  $userId = $mysqli->real_escape_string($_POST['user_id']);
-  $sql = "DELETE FROM user WHERE id = $userId";
-  if ($mysqli->query($sql)) {
-    echo 'success';
-  } else {
-    echo 'error';
-  }
-  $mysqli->close();
+    $mysqli = require __DIR__ . '/database.php';
+    $userId = $mysqli->real_escape_string($_POST['user_id']);
+    $sql = "DELETE FROM user WHERE id = '$userId'";
+    if ($mysqli->query($sql) === TRUE) {
+        echo 'success';
+    } else {
+        echo 'Failed to delete user: ' . $mysqli->error;
+    }
+    $mysqli->close();
 }
 ?>
