a015aaa0bcf788fe910534ec212eca94ccef6b8d
tushan-shahid <shahidshabraj@gmail.com>
2023-03-31 13:02:19 -0700

    layout fixes designs changed

--- fullPost.php
+++ fullPost.php
@@ -129,30 +129,32 @@ if (isset($_GET['like']) && $_GET['like'] == '1') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <style>
-        .navbar-nav .nav-link:hover {
-            background-color: #3d464e;
-            border-radius: 10%;
+    .navbar-nav .nav-link:hover {
+        background-color: #3d464e;
+        border-radius: 10%;
 
-        }
+    }
 
-        .btn:hover {
-            background-color: #f1f1f1;
-        }
+    .btn:hover {
+        background-color: #f1f1f1;
+    }
 
-        .btn i {
-            margin-right: 5px;
-        }
+    .btn i {
+        margin-right: 5px;
+    }
     </style>
 
     <title>Full-Post</title>
     <link rel="stylesheet" href="path/to/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
 
     <script src="https://kit.fontawesome.com/ff48066121.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
 
     <link rel="stylesheet" href="./css/styles.css" />
@@ -162,7 +164,9 @@ if (isset($_GET['like']) && $_GET['like'] == '1') {
             <a class="navbar-brand" href="#">
                 <img src="./img/logo.png" alt="..." height="80" />
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -190,53 +194,70 @@ if (isset($_GET['like']) && $_GET['like'] == '1') {
             <p class="lead my-3">Read entire content here, like and comment</p>
             <div>
 
-                <button class="btn btn-primary" onclick="window.location.href='postsFeed.php'"><i class=" bi bi-journal-text"></i>
-                    Posts Feed</button> <button class="btn btn-primary me-2" onclick="window.location.href='userHomepage.php'"><i class="bi bi-house"></i> Home</button>
+                <button class="btn btn-primary" onclick="window.location.href='postsFeed.php'"><i
+                        class=" bi bi-journal-text"></i>
+                    Posts Feed</button> <button class="btn btn-primary me-2"
+                    onclick="window.location.href='userHomepage.php'"><i class="bi bi-house"></i> Home</button>
             </div>
         </div>
-        <div class="container border rounded">
-            <div class="row">
-                <div class="col-md-6">
-                    <img src="<?php echo $image; ?>" class="img-fluid" alt="Post Image" />
-                </div>
-                <div class="col-md-6">
-                    <h1 class="mt-3"><?php echo $title; ?></h1>
-                    <p class="mb-2"><?php echo $date; ?></p>
-                    <?php
-                    if (!isset($profilepicture) || empty($profilepicture)) {
-                        $profilepicture = './img/userProimg.jpg'; // set default profile picture
-                    }
-                    ?>
-                    <img src="<?php echo $profilepicture; ?>" class="img-fluid rounded-circle mb-2" alt="User Image" style="width: 50px; height: 50px;" />
-                    <p class="mb-2">By <?php echo $username; ?></p>
 
-                    <div class="d-flex align-items-center">
-                        <p class="me-3">Likes: <?php echo get_likes_count($mysqli, $id); ?></p>
-                        <p>Comments: <?php echo count($comments); ?></p>
+        <div class="row justify-content-center">
+            <div class="col-lg-8 col-md-10">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <div class="row mb-4">
+                            <div class="col-md-6">
+                                <img src="<?php echo $image; ?>" class="img-fluid rounded" alt="Post Image" />
+                            </div>
+                            <div class="col-md-6">
+                                <h1 class="mb-4"><?php echo $title; ?></h1>
+                                <div class="d-flex align-items-center mb-3">
+                                    <?php
+                                if (!isset($profilepicture) || empty($profilepicture)) {
+                                    $profilepicture = './img/userProimg.jpg'; // set default profile picture
+                                }
+                                ?>
+                                    <img src="<?php echo $profilepicture; ?>" class="img-fluid rounded-circle me-3"
+                                        alt="User Image" style="width: 50px; height: 50px;" />
+                                    <div>
+                                        <p class="mb-1">By <?php echo $username; ?></p>
+                                        <p class="mb-0"><?php echo $date; ?></p>
+                                    </div>
+                                </div>
+                                <div class="d-flex align-items-center mb-4">
+                                    <p class="me-3 mb-0"><i class="bi bi-hand-thumbs-up"></i>
+                                        <?php echo get_likes_count($mysqli, $id); ?></p>
+                                    <p class="mb-0"><i class="bi bi-chat-dots"></i> <?php echo count($comments); ?>
+                                        Comments</p>
+                                </div>
+                                <?php if (has_user_liked_post($mysqli, $_SESSION['user_id'], $id)) : ?>
+                                <button class="btn btn-primary me-3" disabled><i class="bi bi-hand-thumbs-up"></i>
+                                    Liked</button>
+                                <?php else : ?>
+                                <button class="btn btn-outline-primary me-3" onclick="likePost()"><i
+                                        class="bi bi-hand-thumbs-up"></i> Like</button>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                        <hr>
+                        <p class="card-text"><?php echo $content; ?></p>
                     </div>
-                    <?php if (has_user_liked_post($mysqli, $_SESSION['user_id'], $id)) : ?>
-                        <button class="btn btn-primary me-3" disabled><i class="bi bi-hand-thumbs-up"></i> Liked</button>
-                    <?php else : ?>
-                        <button class="btn btn-outline-primary me-3" onclick="likePost()"><i class="bi bi-hand-thumbs-up"></i> Like</button>
-                    <?php endif; ?>
-
-
-                    <hr>
-                    <p><?php echo $content; ?></p>
                 </div>
             </div>
         </div>
+
+
 
         <div class="container my-5">
             <h2>Comments</h2>
             <?php foreach ($comments as $comment) : ?>
-                <div class="card my-3">
-                    <div class="card-body">
-                        <h5 class="card-title"><?php echo $comment['username']; ?></h5>
-                        <h6 class="card-subtitle mb-2 text-muted"><?php echo $comment['date']; ?></h6>
-                        <p class="card-text"><?php echo $comment['content']; ?></p>
-                    </div>
+            <div class="card my-3">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $comment['username']; ?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?php echo $comment['date']; ?></h6>
+                    <p class="card-text"><?php echo $comment['content']; ?></p>
                 </div>
+            </div>
             <?php endforeach; ?>
 
             <hr>
@@ -255,17 +276,17 @@ if (isset($_GET['like']) && $_GET['like'] == '1') {
         </div>
     </main>
     <style>
-        img {
-            max-height: 400px;
-            margin-top: 20px;
-            border-radius: 5px;
-        }
+    img {
+        max-height: 400px;
+        margin-top: 20px;
+        border-radius: 5px;
+    }
     </style>
     <script>
-        // like post
-        function likePost() {
-            location.href = 'fullPost.php?id=<?php echo $id; ?>&like=1';
-        }
+    // like post
+    function likePost() {
+        location.href = 'fullPost.php?id=<?php echo $id; ?>&like=1';
+    }
     </script>
 
 </body>
--- posts.php
+++ posts.php
@@ -24,12 +24,10 @@ $mysqli->close();
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>posts</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
     <script src="https://kit.fontawesome.com/ff48066121.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
 
     <link rel="stylesheet" href="./css/styles.css" />
@@ -38,9 +36,7 @@ $mysqli->close();
             <a class="navbar-brand" href="#">
                 <img src="./img/logo.png" alt="..." height="80" />
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -75,31 +71,30 @@ $mysqli->close();
 
         <div class="input-group mb-3 justify-content-center">
             <form action="" method="get" class="d-flex">
-                <input type="text" class="form-control px-4 " placeholder="Search posts by title..." name="search"
-                    value="<?php echo $search ?>">
+                <input type="text" class="form-control px-4 " placeholder="Search posts by title..." name="search" value="<?php echo $search ?>">
                 <button class="btn btn-outline-secondary  ms-2" type="submit">Search</button>
             </form>
         </div>
         <div class="row mb-2 align-items-stretch">
             <?php foreach ($posts as $post) : ?>
-            <div class="col-md-6">
-                <div
-                    class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                    <div class="col p-4 d-flex flex-column position-static postUser">
-                        <h3 class="mb-0"><?php echo $post["title"]; ?></h3>
-                        <?php echo substr($post['content'], 0, 100) . '...'; ?>
-                        <a href="signup.php" class="stretched-link">Continue reading</a>
-                        <div class="mt-3 d-flex align-items-center">
-                            <span class="me-4"><i class="bi bi-heart text-danger hover-text-danger"></i></span>
-                            <span class="ms-4"><i class="bi bi-chat-dots text-primary hover-text-primary"></i></span>
+                <div class="col-md-6">
+                    <div class="row g-0 border bg-white rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div class="col p-4 d-flex flex-column position-static postUser">
+                            <h3 class="mb-0" style="display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; overflow: hidden;">
+                                <?php echo $post["title"]; ?></h3>
+                            <?php echo substr($post['content'], 0, 100) . '...'; ?>
+                            <a href="signup.php" class="stretched-link">Continue reading</a>
+                            <div class="mt-3 d-flex align-items-center">
+                                <span class="me-4"><i class="bi bi-heart text-danger hover-text-danger"></i></span>
+                                <span class="ms-4"><i class="bi bi-chat-dots text-primary hover-text-primary"></i></span>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-6 col-lg-4 d-flex align-items-center p-2">
-                        <img src="<?php echo $post["image"]; ?>" alt="User Image" class="img-fluid" />
-                    </div>
+                        <div class="col-md-6 col-lg-4 d-flex align-items-center p-2">
+                            <img src="<?php echo $post["image"]; ?>" alt="User Image" class="img-fluid" />
+                        </div>
 
+                    </div>
                 </div>
-            </div>
             <?php endforeach; ?>
         </div>
     </main>
