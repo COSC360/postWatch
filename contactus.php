c2428f936fcc1f02213a919d8ac676ca18b09a76
tushan-shahid <shahidshabraj@gmail.com>
2023-04-03 20:55:46 -0700

    fixes

--- contactus.php
+++ contactus.php
@@ -5,9 +5,8 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Admin Signup Request</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
+    <title>Contact Us</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
 </head>
 
 <body>
@@ -16,8 +15,7 @@
             <a class="navbar-brand" href="#">
                 <img src="./img/logo.png" alt="Logo" height="80" class="d-inline-block align-text-top" />
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
@@ -31,7 +29,7 @@
     </nav>
 
     <div class="container my-5">
-        <h1 class="text-center mb-4">Admin Signup Request</h1>
+        <h1 class="text-center mb-4">Contact Us</h1>
         <div class="row">
             <div class="col-md-6 offset-md-3">
                 <form>
@@ -45,8 +43,7 @@
                     </div>
                     <div class="form-group">
                         <label for="phone">Phone Number</label>
-                        <input type="tel" class="form-control" id="phone" placeholder="Enter your phone number"
-                            required />
+                        <input type="tel" class="form-control" id="phone" placeholder="Enter your phone number" required />
                     </div>
 
 
@@ -55,42 +52,41 @@
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
     <script>
-    function validateForm() {
-        let name = document.getElementById("name").value.trim();
-        let email = document.getElementById("email").value.trim();
-        let phone = document.getElementById("phone").value.trim();
+        function validateForm() {
+            let name = document.getElementById("name").value.trim();
+            let email = document.getElementById("email").value.trim();
+            let phone = document.getElementById("phone").value.trim();
 
-        // Check if name is empty
-        if (name === "") {
-            alert("Please enter your name.");
-            return false;
+            // Check if name is empty
+            if (name === "") {
+                alert("Please enter your name.");
+                return false;
+            }
+
+            // Check if email is empty or invalid
+            if (email === "") {
+                alert("Please enter your email.");
+                return false;
+            } else if (!/\S+@\S+\.\S+/.test(email)) {
+                alert("Please enter a valid email address.");
+                return false;
+            }
+
+            // Check if phone number is empty or invalid
+            if (phone === "") {
+                alert("Please enter your phone number.");
+                return false;
+            } else if (!/^\d{10}$/.test(phone)) {
+                alert("Please enter a valid 10-digit phone number.");
+                return false;
+            }
+
+            // If all validation passes, submit the form
+            return true;
         }
-
-        // Check if email is empty or invalid
-        if (email === "") {
-            alert("Please enter your email.");
-            return false;
-        } else if (!/\S+@\S+\.\S+/.test(email)) {
-            alert("Please enter a valid email address.");
-            return false;
-        }
-
-        // Check if phone number is empty or invalid
-        if (phone === "") {
-            alert("Please enter your phone number.");
-            return false;
-        } else if (!/^\d{10}$/.test(phone)) {
-            alert("Please enter a valid 10-digit phone number.");
-            return false;
-        }
-
-        // If all validation passes, submit the form
-        return true;
-    }
     </script>
 </body>
 
